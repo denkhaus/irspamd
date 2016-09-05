1c7b4bc5f24da062a89c9423477810502bac8597
denkhaus <peristaltic@gmx.net>
2016-09-05 21:36:54 +0200

    remove datastore artifacts

--- engine/engine.go
+++ engine/engine.go
@@ -1,12 +1,5 @@
 package engine
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/denkhaus/tcgl/applog"
-)
-
 ////////////////////////////////////////////////////////////////////////////////
 type Engine struct {
 }
@@ -24,32 +17,6 @@ type EngineFunc func(engine *Engine) error
 ////////////////////////////////////////////////////////////////////////////////
 func (e *Engine) Execute(fn EngineFunc) error {
 	return fn(e)
-}
-
-////////////////////////////////////////////////////////////////////////////////
-func (e *Engine) initDataStore(reset bool, args ...interface{}) (*DataStore, error) {
-	dbPath, err := getDBPathByArgs(args...)
-	if err != nil {
-		return nil, fmt.Errorf("Store::GetDBPathByArgs::%s", err)
-	}
-
-	if reset {
-		applog.Infof("Store::Reset database %s", dbPath)
-		os.Remove(dbPath)
-	}
-
-	store, err := NewDatastore(dbPath, "UIDMap")
-	if err != nil {
-		return nil, fmt.Errorf("Store::%s", err)
-	}
-
-	if reset {
-		applog.Infof("Store::Start with new database %s", dbPath)
-	} else {
-		applog.Infof("Store::Use database %s", dbPath)
-	}
-
-	return store, nil
 }
 
 ////////////////////////////////////////////////////////////////////////////////
